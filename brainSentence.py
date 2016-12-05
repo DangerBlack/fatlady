9bdec55880b5eac11a64cc86ecb367c6c74cebf9
DangerBlack <danger.recheng@hotmail.it>
2016-12-05 16:59:46 +0100

    update mp3 managment server side, need to implement the client side

--- brainSentence.py
+++ brainSentence.py
@@ -15,14 +15,14 @@ class BrainSentence:
             }
 
     action = {
-            "saluta":["Hello [], nice to meet you, I'm Amros Black named as the raven.","none"],
-            "introduci":["introduci","happy"], #I'm Amaros Black from the glorius Black family, I'm a famous painter in my time, and now I'm just hanging around. Hoping not to have an hanghover...
-            "newyear":["happy new year","happy"],
-            "painting":["I'm painting a naked lady","none"],#I'm just painting a naked lady, but it's hard sometimes remembering her face
-            "no":["Absolutely no","none"],
-            "lose":["Any prefect around I wanna slipt 10 point to this ugly guy","angry"],
-            "riddle":["How many uses of dragon blood did DUmbledore discover?","none"],
-            "correct":["you are right! good boy","happy"]
+            "saluta":["salutaNAME.mp3","none"],
+            "introduci":["introduci.mp3","happy"], #I'm Amaros Black from the glorius Black family, I'm a famous painter in my time, and now I'm just hanging around. Hoping not to have an hanghover...
+            "newyear":["happynewyear.mp3","happy"],
+            "painting":["painting.mp3","none"],#I'm just painting a naked lady, but it's hard sometimes remembering her face
+            "no":["no.mp3","none"],
+            "lose":["givepoint.mp3","angry"],
+            "riddle":["riddle.mp3","none"],
+            "correct":["correct.mp3","happy"]
 
     }
 
@@ -31,20 +31,35 @@ class BrainSentence:
     def __init__(self, sentence):
         self.sentence = sentence
 
+
+    #Return a list of two [AUDIO_FILE_NAME,SPEAKING_EMOTION]
+    #using the AUDIO_FILE_NAME you can reproduce those file
+    #using the emotion you can better chose the correct scene
     def deduce(self):
+        selected_verb = ''
+        selected_name = ''
+        selected_action = ''
         for v in self.verbs.keys():
             if(v in self.sentence):
                 hasName = False
                 for n in self.names:
                     if(n in self.sentence):
                         hasName=True
-                        print 'trovato['+v+','+n+']: '+self.verbs[v]+" -> "+n
+                        selected_verb = self.verbs[v]
+                        selected_name = n
+                        #print 'trovato['+v+','+n+']: '+self.verbs[v]+" -> "+n
                 if(not hasName):
-                    print 'trovato['+v+']: '+self.verbs[v]
+                    #print 'trovato['+v+']: '+self.verbs[v]
+                    selected_verb = self.verbs[v]
+        if("NAME" in self.action[selected_verb][0]):
+            selected_action=[self.action[selected_verb][0].replace("NAME",selected_name),self.action[selected_verb][1]]
+        else:
+            selected_action=self.action[selected_verb]
+        return selected_action
 
 bs = BrainSentence('who are you')
-bs.deduce()
-bs = BrainSentence("hi i'm danger")
-bs.deduce()
-bs = BrainSentence("hi i am danger")
-bs.deduce()
+print(bs.deduce())
+bs = BrainSentence("hi i'm claudia")
+print(bs.deduce())
+bs = BrainSentence("hi i am claudia")
+print(bs.deduce())
